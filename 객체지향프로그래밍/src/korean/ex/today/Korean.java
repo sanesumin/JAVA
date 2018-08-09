5f158ffca7bfa9c73b214ba4ffa39a99907c5b1a
1-3 <1-3@1-3-PC>
2018-08-09 16:19:58 +0900

    객체지향프로그래밍

---
+++ 객체지향프로그래밍/src/korean/ex/today/Korean.java
@@ -0,0 +1,16 @@
+package korean.ex.today;
+
+public class Korean {
+	String nation = "¥Î«—πŒ±π";
+	String name;
+	String ssn;
+	
+	public Korean() {
+		super();
+	}
+	
+	public Korean(String name, String ssn) {
+		this.name = name;
+		this.ssn = ssn;
+	}
+}
---
+++ 객체지향프로그래밍/src/student/today/Student.java
@@ -0,0 +1,20 @@
+package student.today;
+
+public class Student {
+	
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		System.out.println("s1 변수가 Student 객체를 참조합니다.");
+		
+		
+		Student s2 = new Student();
+		System.out.println("s2 변수가 Student 또다른 객체를 참조합니다.");
+		
+		if(s1==s2) {
+			System.out.println("주소가 같다.");
+		}else {
+			System.out.println("주소가 다르다.");
+		}
+
+	}
+}
