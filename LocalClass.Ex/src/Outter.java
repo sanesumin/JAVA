82e0df3dc0899467039c33344d5da21ccb0425ab
1-3 <1-3@1-3-PC>
2018-08-09 16:27:38 +0900

    LocalClass.Ex

---
+++ LocalClass.Ex/src/Outter.java
@@ -0,0 +1,25 @@
+
+public class Outter {
+	
+	public void method1(final int arg) {
+		final int local	Variable = 1;
+		//arg = 100;
+		//localVariable = 100;
+		class Inner{
+			public void method() {
+				int result = arg + localVariable;
+			}
+		}
+	}
+	
+	public void method2(int arg) {
+		int localVariable = 1;
+		//arg = 100;
+		//localVariable = 100;
+		class Inner{
+			public void method() {
+				int result = arg + localVariable;
+			}
+		}
+	}
+}
